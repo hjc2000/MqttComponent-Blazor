b748eba543141a31e4bc58fdde04f84e32181354
hjc2000 <huangjincan163@163.com>
2022-05-02 20:03:11 +0800

    1

--- Mqtt.razor.js
+++ Mqtt.razor.js
@@ -11,22 +11,37 @@
         let options = {
             // Clean session
             clean: true,
+            connectTimeout: 3000,
+            keepalive: 15,
             // Auth
             username,
             password,
         }
         this.dotnetHelper = dotnetHelper;
         this.client = mqtt.connect("ws://127.0.0.1:8083/mqtt", options);
-        if (this.client.connected)
+        this.b_HadBeenConnected = false;//过去曾经连接成功过
+        this.client.on("reconnect", () =>
         {
-            console.log("连接成功");
-        }
-        else
-        {
-            this.dotnetHelper.invokeMethodAsync("OnConnectFail");
-        }
+            /**
+             * 请求被emqx拒绝后并不会进入 error 事件，而是不断进入
+             * reconnect 事件
+             * 没办法，只能通过这个事件来通知用户连接失败。为了区分到底是连接失
+             * 败还是短线重连，需要设置一个标志位 b_HadBeenConnected
+             */
+            if (!this.b_HadBeenConnected)
+            {
+                //触发 .NET 的连接失败事件
+                this.dotnetHelper.invokeMethodAsync("OnConnectFail");
+                this.client.end(true);//关闭客户端，防止一直重连
+            }
+            else
+            {
+                console.log("重连");
+            }
+        });
         this.client.on("connect", () =>
         {
+            this.b_HadBeenConnected = true;
             this.dotnetHelper.invokeMethodAsync("OnConnected");
         });
         //在收到数据后将数据传给.NET方法
