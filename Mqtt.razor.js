c5085be51a4e03ead59313e660cc4ae1df63262f
hjc2000 <huangjincan163@163.com>
2022-05-02 19:37:39 +0800

    1

--- Mqtt.razor.js
+++ Mqtt.razor.js
@@ -1,10 +1,13 @@
-﻿class Mqtt {
+﻿class Mqtt
+{
     /**
      * 如果没有传递参数，则会使用匿名连接
      * @param {string} username
      * @param {string} password
      */
-    constructor(dotnetHelper, username, password) {
+    constructor(dotnetHelper, username, password)
+    {
+        console.log(password);
         let options = {
             // Clean session
             clean: true,
@@ -12,21 +15,26 @@
             username,
             password,
         }
-
         this.dotnetHelper = dotnetHelper;
-
         this.client = mqtt.connect("ws://127.0.0.1:8083/mqtt", options);
-
-        this.client.on("connect", () => {
+        if (this.client.connected)
+        {
+            console.log("连接成功");
+        }
+        else
+        {
+            this.dotnetHelper.invokeMethodAsync("OnConnectFail");
+        }
+        this.client.on("connect", () =>
+        {
             this.dotnetHelper.invokeMethodAsync("OnConnected");
-            });
-
+        });
         //在收到数据后将数据传给.NET方法
-        this.client.on("message", (topic, payload) => {
+        this.client.on("message", (topic, payload) =>
+        {
             payload = Uint8Array.from(payload.toJSON().data);
             this.dotnetHelper.invokeMethodAsync("OnReceived", topic, payload);
         });
-
     }
 
     /**
@@ -34,38 +42,48 @@
      * @param {string} topic
      * @param {Uint8Array} payload
      */
-    publish(topic, payload) {
+    publish(topic, payload)
+    {
         this.client.publish(topic, payload);
     };
 
-    subscribe(topic) {
-        this.client.subscribe(topic, (error, granted) => {
-            if (error) {
+    subscribe(topic)
+    {
+        this.client.subscribe(topic, (error, granted) =>
+        {
+            if (error)
+            {
                 console.log(error);
-            } else {
+            } else
+            {
                 console.log("订阅了" + granted[0].topic);
             }
         });
     }
 }
 
-export function getMqtt(dotnetHelper) {
-    return new Mqtt(dotnetHelper, "hjc", "123456");
+export function getMqtt(dotnetHelper, username, password)
+{
+    return new Mqtt(dotnetHelper, username, password);
 }
 
 /**
  * 从CDN获取mqtt.js并加载，加载完成后触发事件，调用 .NET 方法
  * @param {any} dotnetHelper
  */
-export function installMqtt(dotnetHelper) {
-    if (window.mqttInstalled === true) {
+export function installMqtt(dotnetHelper)
+{
+    if (window.mqttInstalled === true)
+    {
         dotnetHelper.invokeMethodAsync("OnInstalled");
-    } else {
+    } else
+    {
         let script = document.createElement('script');
         //script.src = "https://unpkg.com/mqtt/dist/mqtt.min.js";
         script.src = "/js/mqtt.min.js";
         script.async = true;
-        script.onload = () => {
+        script.onload = () =>
+        {
             dotnetHelper.invokeMethodAsync("OnInstalled");
         }
         document.head.append(script);
